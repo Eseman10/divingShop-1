658d4fd0e5b69a2987277fd8a9194f3337c9f016
Eseman10 <eseman10@yahoo.com>
2022-04-25 15:31:53 +0200

    updated

--- frontend/src/components/mee.jsx
+++ frontend/src/components/mee.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import {useEffect} from 'react';
+
+
 
 const mee = () => {
+
+    useEffect(() => {
+        console.log('mee');
+    }, []);
+
+
   return (
-    <div>mee</div>
+    <div className="maa" >mee</div>
   )
 }
 
